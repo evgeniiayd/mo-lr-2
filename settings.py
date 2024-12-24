96c2969ac9063f3b51bdd5687e6f2e2163244869
evgeniiayd <evgeniiappletree@mail.ru>
2024-12-24 18:19:45 +0300

    commit all

---
+++ settings.py
@@ -0,0 +1,46 @@
+class Settings():
+    """Настройки"""
+
+    def __init__(self):
+        self.width = 1200
+        self.height = 720
+        self.bg_color = (255, 203, 219)
+        self.bullets_allowed = 3
+
+        # Настройки корабля
+        self.ship_speed = 1.5
+        self.ship_limit = 3
+
+        # Параметры снаряда
+        self.bullet_speed = 1.5
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+
+        # Настройки пришельцев
+        self.alien_speed = 0.5
+        self.fleet_drop_speed = 5
+        self.fleet_direction = 1
+
+        # Темп ускорения игры
+        self.speedup_scale = 1.1
+        self.score_scale = 1.5
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Initialize settings that change throughout the game."""
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3
+        self.alien_speed_factor = 1
+
+        # fleet_direction of 1 represents right, -1 represents left.
+        self.fleet_direction = 1
+        self.alien_points = 50
+
+    def increase_speed(self):
+        """Increase speed settings and alien point values."""
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
---
+++ ship.py
@@ -0,0 +1,41 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Ship(Sprite):
+
+    def __init__(self, ai_game):
+        super().__init__()
+        """Инициализирует корабль и задает его начальную позицию"""
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # Загружает изображение корабля и получает прямоугольник
+        self.image = pygame.image.load('images/ship2.png')
+        self.rect = self.image.get_rect()
+
+        # Каждый новый корабль появляется у нижнего края экрана
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
+
+        # Флаг перемещения
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Обновляет позицию короабля с учетом флагов"""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        self.rect.x = self.x
+
+    def blitme(self):
+        """Рисует корабль в текущей позиции"""
+        self.screen.blit(self.image, self.rect)
+
+    def center_ship(self):
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
